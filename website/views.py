eb8a3366010a3a6054d235d299fcd2338faa5d34
VanessaTorgler <vtorgler@gmail.com>
2025-10-01 14:32:20 +1000

    added event and booking history extensions

--- website/views.py
+++ website/views.py
@@ -4,4 +4,11 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/home')
 def index():
-    return render_template('index.html')  
+    return render_template('index.html', active_page='home')  
+@main_bp.route('/event')
+def event():
+    return render_template('event.html', active_page='event')
+
+@main_bp.route('/bookinghistory')
+def bookinghistory():
+    return render_template('history.html', active_page='bookinghistory')
