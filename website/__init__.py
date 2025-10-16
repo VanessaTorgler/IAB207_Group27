99f41eba58855a94cfbeec9844adfa9b0cdccf65
Meer7081 <meer.7081@icloud.com>
2025-10-16 23:46:00 +1000

    register bookings blueprint

--- website/__init__.py
+++ website/__init__.py
@@ -52,5 +52,8 @@ def create_app():
 
     from . import auth
     app.register_blueprint(auth.auth_bp)
+
+    from . import bookings
+    app.register_blueprint(bookings.bookings_bp)
     
     return app
